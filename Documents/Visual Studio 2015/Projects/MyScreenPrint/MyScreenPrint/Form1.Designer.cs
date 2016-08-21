992ee4a2b5e0e6a571fe5b853e5489215904c2bf
lc1995 <498994046@qq.com>
2016-08-21 12:29:40 +0800

    A simple screen catch

---
+++ Documents/Visual Studio 2015/Projects/MyScreenPrint/MyScreenPrint/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MyScreenPrint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.screenPic = new System.Windows.Forms.PictureBox();
+            this.operationPanel = new System.Windows.Forms.Panel();
+            this.printScrBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.screenPic)).BeginInit();
+            this.operationPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // screenPic
+            // 
+            this.screenPic.BackColor = System.Drawing.Color.Black;
+            this.screenPic.Location = new System.Drawing.Point(21, 12);
+            this.screenPic.Name = "screenPic";
+            this.screenPic.Size = new System.Drawing.Size(1045, 606);
+            this.screenPic.TabIndex = 0;
+            this.screenPic.TabStop = false;
+            // 
+            // operationPanel
+            // 
+            this.operationPanel.Controls.Add(this.printScrBtn);
+            this.operationPanel.Location = new System.Drawing.Point(1072, 12);
+            this.operationPanel.Name = "operationPanel";
+            this.operationPanel.Size = new System.Drawing.Size(109, 606);
+            this.operationPanel.TabIndex = 1;
+            // 
+            // printScrBtn
+            // 
+            this.printScrBtn.Location = new System.Drawing.Point(4, 4);
+            this.printScrBtn.Name = "printScrBtn";
+            this.printScrBtn.Size = new System.Drawing.Size(102, 48);
+            this.printScrBtn.TabIndex = 0;
+            this.printScrBtn.Text = "Print Screen";
+            this.printScrBtn.UseVisualStyleBackColor = true;
+            this.printScrBtn.Click += new System.EventHandler(this.printScrBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1193, 630);
+            this.Controls.Add(this.operationPanel);
+            this.Controls.Add(this.screenPic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.screenPic)).EndInit();
+            this.operationPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox screenPic;
+        private System.Windows.Forms.Panel operationPanel;
+        private System.Windows.Forms.Button printScrBtn;
+    }
+}
+
---
+++ Documents/Visual Studio 2015/Projects/MyScreenPrint/MyScreenPrint/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MyScreenPrint
+{
+    public partial class Form1 : Form
+    {
+        // 截图窗口
+        Cutter cutter = null;
+
+        // 截得的图片
+        public static Bitmap catchBmp = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 点击按钮开始捕捉屏幕
+        private void printScrBtn_Click(object sender, EventArgs e)
+        {
+            // 新建一个截图窗口
+            cutter = new Cutter();
+
+            // 隐藏原窗口
+            Hide();
+            Thread.Sleep(200);
+
+            // 设置截图窗口的背景图片
+            Bitmap bmp = new Bitmap(Screen.AllScreens[0].Bounds.Width, Screen.AllScreens[0].Bounds.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(new Point(0, 0), new Point(0, 0), new Size(bmp.Width, bmp.Height));
+            cutter.BackgroundImage = bmp;
+
+            // 显示原窗口
+            Show();
+
+            // 显示截图窗口
+            cutter.WindowState = FormWindowState.Maximized;
+            cutter.ShowDialog();
+
+            UpdateScreen();
+        }
+
+        // 更新图片显示
+        private void UpdateScreen()
+        {
+            if (catchBmp != null)
+                screenPic.Image = catchBmp;
+        }
+    }
+}
